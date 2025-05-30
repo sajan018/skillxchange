889a645f6e8bea8365a1dc4142952df83a8fa3b0
Sajan Kumar <sa102515.psjs@gmail.com>
2025-05-30 16:01:23 +0530

    first commit

---
+++ src/main/java/com/myproject/skillxchange/SkillxchangeApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.skillxchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillxchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillxchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/skillxchange/SkillxchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.skillxchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillxchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
